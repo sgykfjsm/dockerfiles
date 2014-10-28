d150e8e2a4924014d09a0bb3cc285d510bc96c44
shigeyuki.fujishima <shigeyuki.fujishima@gmail.com>
2014-10-29 03:54:08 +0900

    add unit testing for simple nginx setting

--- setup.sh
+++ setup.sh
@@ -1,32 +1,46 @@
 #!/bin/bash
 
-apt-get -y update
-apt-get -y upgrade
-apt-get -y install git gcc mercurial liblua5.2-dev liblua5.2-0 libncurses5-dev luajit libluajit-5.1-dev
-apt-get clean && apt-get clean && rm -rf /var/cache/apt/archives/* /var/lib/apt/lists/*
+set -eu
 
-# gist setting
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+locale-gen en_US.UTF-8
+dpkg-reconfigure locales
 
-## https://gist.github.com/sgykfjsm/8426961
-curl --silent https://gist.githubusercontent.com/sgykfjsm/8426961/raw/f5d155e14d295c1fa0803957e5041c83f6fb4f7f/.vimrc > /home/vagrant/.vimrc
+apt-get -qq -y update && apt-get -qq -y upgrade
+apt-get -qq -y install git gcc mercurial liblua5.2-dev liblua5.2-0 libncurses5-dev luajit libluajit-5.1-dev
+apt-get clean && rm -rf /var/cache/apt/archives/* /var/lib/apt/lists/*
 
-## https://gist.github.com/sgykfjsm/9845504
-curl --silent https://gist.githubusercontent.com/sgykfjsm/9845504/raw/4d403494a87d227094da4a2f8215f6c2ff1d5438/bash_profile > /home/vagrant/.bash_profile
-echo 'export PATH=${PATH}:/usr/local/bin:${HOME}/local/bin' >> /home/vagrant/.bash_profile
+# setting from gist
 
-hg clone -r 0446fa17bd95cdb37cd0c14ada32818b43577597 https://vim.googlecode.com/hg/ /usr/local/src/vim7.4.475
-cd /usr/local/src/vim7.4.475
-./configure --enable-multibyte --with-features=huge --disable-selinux --prefix=/usr/local --enable-luainterp=yes --with-lua-prefix=/usr --disable-darwin -with-luajit  --enable-fail-if-missing
-make && sudo make install
+# ## https://gist.github.com/sgykfjsm/8426961
+# curl --silent https://gist.githubusercontent.com/sgykfjsm/8426961/raw/f5d155e14d295c1fa0803957e5041c83f6fb4f7f/.vimrc > /home/vagrant/.vimrc
+#
+# ## https://gist.github.com/sgykfjsm/9845504
+# curl --silent https://gist.githubusercontent.com/sgykfjsm/9845504/raw/4d403494a87d227094da4a2f8215f6c2ff1d5438/bash_profile > /home/vagrant/.bash_profile
+# echo 'export PATH=/usr/local/bin:${HOME}/local/bin:${PATH}' >> /home/vagrant/.bash_profile
+#
+# vim_src=/usr/local/src/vim7.4.475
+# rm -rf ${vim_src}
+# hg clone -r 0446fa17bd95cdb37cd0c14ada32818b43577597 https://vim.googlecode.com/hg/ ${vim_src}
+# cd ${vim_src}
+# ./configure --enable-multibyte --with-features=huge --disable-selinux --prefix=/usr/local --enable-luainterp=yes --with-lua-prefix=/usr --disable-darwin -with-luajit  --enable-fail-if-missing
+# make && sudo make install
+#
+# vundle_dir=/home/vagrant/.vim/bundle/Vundle.vim
+# rm -rf ${vundle_dir}
+# git clone https://github.com/gmarik/Vundle.vim.git ${vundle_dir}
 
-git clone https://github.com/gmarik/Vundle.vim.git /home/vagrant/.vim/bundle/Vundle.vim
+chown -R vagrant:vagrant /home/vagrant/.
 
-chown -R vagrant:vagrant /home/vagrant
+curl -sSL https://get.docker.com/ubuntu/ | sh
+curl --silent https://raw.githubusercontent.com/docker/docker/master/contrib/completion/bash/docker > /etc/bash_completion.d/docker
+source /etc/bash_completion.d/docker
 
-apt-get -y update
-apt-get -y install docker.io
-ln -sf /usr/bin/docker.io /usr/local/bin/docker
-sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
-source /etc/bash_completion.d/docker.io
+echo source /etc/bash_completion.d/docker >> /home/vagrant/.bash_profile
+echo 'DOCKER_OPTS="-H 0.0.0.0:5432"' >> /etc/default/docker
+service docker restart
+sleep 10
 
-echo source /etc/bash_completion.d/docker.io >> /home/vagrant/.bash_profile
+docker -H ":5432" run -v /usr/local/bin:/target jpetazzo/nsenter
