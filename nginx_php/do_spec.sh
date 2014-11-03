f7213b56918df233796eeaf768b3513f6d9d35f2
shigeyuki.fujishima <shigeyuki.fujishima@gmail.com>
2014-11-04 06:35:26 +0900

    make nginx + php5fpm

--- nginx_php/do_spec.sh
+++ nginx_php/do_spec.sh
@@ -4,7 +4,7 @@ set -eu
 
 #
 vagrant_cmd=$(which vagrant)
-vagrant_hostname=docker_tdd
+vagrant_hostname=nginx_php5fpm
 
 #
 ssh_cmd=$(which ssh)
@@ -14,14 +14,15 @@ ssh_opts="-F ${ssh_config_to_vagrant} -q"
 ssh_cmd="${ssh_cmd} ${ssh_opts}"
 
 #
-docker_cmd=$(ssh -F ${ssh_config_to_vagrant} docker_tdd 'which docker')
+docker_cmd=$(ssh -F ${ssh_config_to_vagrant} ${vagrant_hostname} 'which docker')
 docker_opts="-H :5432"
 docker_cmd="${docker_cmd} ${docker_opts}"
-docker_share_dir=/opt/docker # on docker host
+docker_share_dir_base=/opt/docker # on docker host
 docker_repository_name=sgykfjsm
-data_container_name=data1
-app_container_name=app1
-
+base_container_name=base_ubuntu
+data_container_name=data
+app_container_name=nginx_php5fpm
+docker_data_dir=${docker_share_dir_base}/${data_container_name}
 #
 rake_cmd=$(which rake)
 
@@ -31,33 +32,32 @@ do
     echo ${cid}
     ${ssh_cmd} ${vagrant_hostname} " \
         ${docker_cmd} stop ${cid} > /dev/null \
-        && ${docker_cmd} rm ${cid} > /dev/null \
-        && sudo rm -rf ${docker_share_dir}/${data_container_name} \
+        && ${docker_cmd} rm ${cid} > /dev/null
     "
 done
+${ssh_cmd} ${vagrant_hostname} "sudo rm -rf ${docker_data_dir}"
 
-echo "Build docker image(${data_container_name}):"
+echo "Pull docker image(${base_container_name}):"
 ${ssh_cmd} ${vagrant_hostname} \
-    "${docker_cmd} build -t ${docker_repository_name}/${data_container_name} /vagrant/docker/data1/."
+    "${docker_cmd} pull ${docker_repository_name}/${base_container_name}"
 echo
 
 echo "Build docker image(${app_container_name}):"
 ${ssh_cmd} ${vagrant_hostname} \
-    "${docker_cmd} build -t ${docker_repository_name}/${app_container_name} /vagrant/docker/nginx/."
+    "${docker_cmd} build -t ${docker_repository_name}/${app_container_name} /vagrant/docker/."
 echo
 
 echo "Run container:"
 ${ssh_cmd} ${vagrant_hostname} <<EOF
-sudo rm -rf ${docker_share_dir}/${data_container_name}
-sudo mkdir -m 755 -p \
-    ${docker_share_dir}/${data_container_name}/var/log/{nginx,monit}
+sudo rm -rf ${docker_data_dir}
+sudo mkdir -m 755 -p ${docker_data_dir}/var/log/{nginx,monit,php}
 
 # data container
 ${docker_cmd} run -i -d -t -P \
     --name ${data_container_name} \
-    -v ${docker_share_dir}/${data_container_name}/var/log:/var/log \
+    -v ${docker_data_dir}/var/log:/var/log \
     -v /etc/localtime:/etc/localtime:ro \
-    ${docker_repository_name}/${data_container_name}
+    ${docker_repository_name}/${base_container_name}
 
 # application container
 ${docker_cmd} run -i -d -t \
@@ -86,17 +86,17 @@ ${ssh_cmd} ${vagrant_hostname} " \
         tar zcf /backup/$(date '+%Y%m%d%H%M%S.%Z')_backup.tar /var/log \
 "
 
-echo "Delete container:"
-
-for cid in $(${ssh_cmd} ${vagrant_hostname} "${docker_cmd} ps -a -q --no-trunc")
-do
-    echo ${cid}
-    ${ssh_cmd} ${vagrant_hostname} " \
-        ${docker_cmd} stop ${cid} > /dev/null \
-        && ${docker_cmd} rm ${cid} > /dev/null \
-        && sudo rm -rf ${docker_share_dir}/${data_container_name} \
-    "
-done
-
+# echo "Delete container:"
+#
+# for cid in $(${ssh_cmd} ${vagrant_hostname} "${docker_cmd} ps -a -q --no-trunc")
+# do
+#     echo ${cid}
+#     ${ssh_cmd} ${vagrant_hostname} " \
+#         ${docker_cmd} stop ${cid} > /dev/null \
+#         && ${docker_cmd} rm ${cid} > /dev/null
+#     "
+# done
+# ${ssh_cmd} ${vagrant_hostname} "sudo rm -rf ${docker_data_dir}"
+#
 rm -f ${ssh_config_to_vagrant}
 exit
